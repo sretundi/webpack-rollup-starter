60087e5dadd2268d19b1d4836b750127a16f2201
sretundi <sretundi@peaksware.com>
2018-10-18 14:41:23 -0600

    fix

--- src/atomicStructure/atoms/inputs/textInput.jsx
+++ src/atomicStructure/atoms/inputs/textInput.jsx
@@ -6,7 +6,6 @@ import styles from './inputs.css';
 export default function(props) {
     return ( 
         <input type='text' 
-            // styleName={`${props.className || ''} b-textInput`}
             className={`${props.className} ${styles['b-textInput']}`}
             value={props.value}
             onChange={(e) => props.onChange(e.target.value)}
